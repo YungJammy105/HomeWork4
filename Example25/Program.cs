c818d12ae4b332ccbef57961e911fb137dca7800
Ilya Andreev <Franklylakers@yandex.ru>
2022-12-05 19:55:09 +0700

    add Example25

---
+++ Example25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Enter number A: ");
+bool intA = int.TryParse(Console.ReadLine(), out int numA);
+Console.Write("Enter number B: ");
+bool intB = int.TryParse(Console.ReadLine(), out int numB);
+if(!(intA & intB))
+{
+    Console.WriteLine("Entered incorrect data");
+}
+else
+{
+    Console.WriteLine(PowerOfNumber(numA,numB));
+}
+
+
+int PowerOfNumber(int number, int power)
+{
+    int result = number;
+    for(int i = 1; i < power; i++)
+    {
+        
+        result = result * number;
+    }
+    return result;
+}
